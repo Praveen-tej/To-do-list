76b1c6353f276ca5b190194f4091177783f741a5
praveen tej <tejpraveen2020@gmail.com>
2022-10-15 17:33:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let myForm = document.getElementById("myForm")
+let myInput = document.getElementById("myInput")
+let myList = document.getElementById("myList")
+
+myForm.addEventListener("submit", (data) => {
+    data.preventDefault()
+    createItem(myInput.value);
+})
+
+createItem = (d) => {
+    let myTemplate = `<li>${d}<button class="button2" onClick = "deleteItem(this)">Delete</button></li>`
+    myList.insertAdjacentHTML("beforeend", myTemplate)
+    myInput.value = ""
+    myInput.focus()
+}
+
+function deleteItem(x) {
+    x.parentElement.remove()
+}
